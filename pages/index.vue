5e391f25d209a24274c9e3350fa64f31dcaca9d8
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2017-12-29 00:58:59 -0600

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,7 @@
         <h3 class="hero__title">What is Jangle?</h3>
         <div class="hero__subtitle hero__body">
           <p class="hero__paragraph">Jangle is a CMS built on top of the popular MongooseJS ODM.</p>
-          <p class="hero__paragraph">It adds functionality in layers, letting us to decide which best pieces fit the problems we want to solve.</p>
+          <p class="hero__paragraph">It adds functionality in layers, letting us decide which best pieces fit the problems we want to solve.</p>
           <p class="hero__paragraph">This project is currently a work-in-progress, but feel free to browse the documentation while you're here.</p>
           <div class="hero__buttons button__row">
             <nuxt-link class="button" to="/docs/core">View documentation</nuxt-link>
